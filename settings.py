1cabbb50aa0e0b121afb43a8b51e1c06081c8950
Артем Огурцов <ogurec98@yandex.ru>
2022-09-09 15:24:23 +0300

    animation and logic

---
+++ settings.py
@@ -0,0 +1,21 @@
+
+
+
+level_map = [
+'                    ',
+'                    ',
+'XX   XXX          XX',
+'XX                  ',
+'XXXX                ',
+'XXXX      P         ',
+'XX       XXXX       ',
+'                    ',
+'   XXXX         XXXX',
+'          XXXXXXXXXX',
+'XXXXXXXXXXXXXXXXXXXX']
+
+tile_size = 64
+
+screen_width = 1200
+screen_height = len(level_map) * tile_size
+
